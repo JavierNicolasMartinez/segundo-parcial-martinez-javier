45dea2191b8945ae763243b67df846b951bef9e3
Javier Nicolas Martinez <javierformosanm@gmail.com>
2025-06-27 10:34:50 -0300

    Conexion con la base de datos y servidor corriendo con exito.

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-import express from 'express';
+import express from "express";
 import dotenv from "dotenv";
+import { router } from "./src/routes/movie.routes.js";
+import { startDB } from "./src/config/database.js";
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
+app.use(express.json());
+app.use("/api", router);
 
-
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
-
-app.listen(PORT, async()=> {
-    console.log("Servidor corriendo con exito en el puerto: ", PORT);
-}
-);
+app.listen(PORT, async() => {
+  await startDB();
+  console.log("Servidor corriendo con exito en el puerto: ", PORT);
+});
