39a4f4ee043772a27bafec25329146a84cd043be
Javier Nicolas Martinez <javierformosanm@gmail.com>
2025-06-27 10:45:59 -0300

    El modelo realizado con sus respectivas caracteristicas.

--- src/models/movie.model.js
+++ src/models/movie.model.js
@@ -1,2 +1,26 @@
 import { DataTypes } from "sequelize";
 
+const movie = sequelize.define("movie", {
+  title: { //título de la pelicula
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  director: { //director
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  duration: { //duración en minutos
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  genre: { //género cinematográfico.
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: { //sipnosis.
+    type: DataTypes.STRING,
+  },
+});
+
+export default movie;
+
